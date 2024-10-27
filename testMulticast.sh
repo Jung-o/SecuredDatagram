c28a17469d05cac57801789483239d4b5ad9ab09
Hugo R <hugo.raps02@gmail.com>
2024-10-27 19:24:07 +0000

    changed the test script to kill processes on SIGINT

--- testMulticast.sh
+++ testMulticast.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
+# Define a cleanup function to kill the background processes
+cleanup() {
+  echo "Stopping the Multicast services..."
+  kill $RECEIVER_PID $SENDER_PID
+  exit 0
+}
+
+# Trap SIGINT (CTRL+C) and call the cleanup function
+trap cleanup SIGINT
+
 cd WorkingTools/TestMulticast || exit
 javac *.java
+
 echo "Testing the Multicast program."
+
 # Start the MulticastReceiver in the background
 java MulticastReceiver 224.0.0.1 8888 &
 RECEIVER_PID=$!
+
 # Give the receiver some time to start
 sleep 2
-# Start the MulticastSender
-java MulticastSender 224.0.0.1 8888 2
-kill $RECEIVER_PID
+
+# Start the MulticastSender in the background
+java MulticastSender 224.0.0.1 8888 1 &
+SENDER_PID=$!
+
+# Wait indefinitely to keep the script running until interrupted
+wait
